9dbc4f03e3c8d172f4d277d73c0ebb874a1fd4ca
Jordan Cloutier <jordan.cloutier@gmail.com>
2023-01-23 15:16:45 -0400

    added changes to footer and minor style changes elsewhere

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
 	return (
 		<div class="p-5 mb-4 bg-light rounded-3">
 			<div class="container-fluid py-5">
-				<h1 class="display-5 fw-bold">Jordan Cloutier</h1>
+				<h1 class="display-4 fw-bold">Jordan Cloutier</h1>
 				<h1 class="display-3 text-secondary fw-bold">your next developer</h1>
 				<p class="col-md-8 fs-4">I am a web developer with a passion for creating beautiful and functional websites, who also has the skill to deliver. Whether you are looking for a simple website or a complex web app, I am confident that I can build something that exceeds expectations.</p>
 				

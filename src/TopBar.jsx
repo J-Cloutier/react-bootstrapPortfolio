102eb69598a2aba0fee69d651d0d228f6bd5baa3
Jordan Cloutier <jordan.cloutier@gmail.com>
2023-02-22 12:49:28 -0400

    changes to top bar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -3,9 +3,9 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Topbar() {
 	return (
-		<Navbar collapseOnSelect expand="lg" bg="secondary" variant="light">
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand href="#home">Jordan Cloutier</Navbar.Brand>
+				<Navbar.Brand className="text-light fs-3" href="#home">Jordan Cloutier</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="ms-auto">
